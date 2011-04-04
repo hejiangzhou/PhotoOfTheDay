77ddc5e3015b90666d9e08760df1bcac7797e0df
Mingliang Liu <liuml07@gmail.com>
2011-04-04 22:31:22 +0800

    Remove the NationalGeographics class
    There are only 3 functions which are the core of this program. I revised the
    code today to find it's not necessary to split them to two classess. Also, I
    added the xdm member to the PhotoOfTheDay class, which indicates the X window
    manager of the environment. Perhaps we can port it to KDE seemlessly now.

--- main.py
+++ main.py
@@ -9,11 +9,23 @@ import urllib2
 from BeautifulSoup import BeautifulSoup
 
 class PhotoOfTheDay:
-    @staticmethod
-    def set():
+    def __init__(self, xdm):
+        self.url = "http://photography.nationalgeographic.com/photography/photo-of-the-day/"
+        self.xdm = xdm
+
+    def today_link(self):
+        page = urllib2.urlopen(self.url)
+        soup = BeautifulSoup(page)
+        for link in soup('div', {'class': 'download_link'}):
+            return link.contents[0]['href']
+        # If no full-size image downloadable, use primary photo
+        for link in soup('div', {'class': 'primary_photo'}):
+            return link.contents[1].contents[1]['src']
+        return None 
+
+    def set_gnome(self):
         client = gconf.client_get_default()
-        ng = NationalGeographic()
-        link = ng.today_link() 
+        link = self.today_link() 
         if not link: return
         data = urllib2.urlopen(link).read()
         filename = "/tmp/photoofthetoday"
@@ -21,20 +33,15 @@ class PhotoOfTheDay:
         client.set_string("/desktop/gnome/background/picture_filename",
                 filename)
 
-class NationalGeographic:
-    def __init__(self):
-        self.url = "http://photography.nationalgeographic.com/photography/photo-of-the-day/"
-    def today_link(self):
-        page = urllib2.urlopen(self.url)
-        soup = BeautifulSoup(page)
-        for link in soup('div', {'class': 'download_link'}):
-            return link.contents[0]['href']
-        for link in soup('div', {'class': 'primary_photo'}):
-            print link.contents[1].contents[1]
-            return link.contents[1].contents[1]['src']
-        return None 
+    def set(self):
+        if not cmp(self.xdm, "GNOME"):
+            self.set_gnome()
+        else:
+            print "Not supported xdm: ", self.xdm
 
 if __name__ == "__main__":
-    PhotoOfTheDay.set()
+    # Set your xdm here, GNOME or KDE
+    potd = PhotoOfTheDay("GNOME")
+    potd.set()
 
 # You may find out no comments for my code. Do you really need them?
