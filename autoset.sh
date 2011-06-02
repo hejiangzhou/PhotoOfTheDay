7332d1485d2eea065948e69bb9b222a934999cc8
hjz <hejiangzhou@gmail.com>
2011-06-02 14:31:34 +0800

    Make autoset.sh able to locate the directory automatically.

--- autoset.sh
+++ autoset.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # run the script daily
-crontab -l | { cat; echo "@daily /dir/to/main.py > /dev/null 2>&1"; } | crontab -
+crontab -l | { cat; echo "@daily $(readlink -f $(dirname $0))/main.py > /dev/null 2>&1"; } | crontab -
 
 # You would like to add this script to the startup application list of Gnome
 # Do it by hand.
